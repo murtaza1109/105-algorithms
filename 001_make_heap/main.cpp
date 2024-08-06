58dc9ba8e21a69075d3b8e06e55864d8391820b3
Sidhpurwala, Murtaza Shabbir <murtazashabbir.sidhpurwala@esko.com>
2024-08-06 21:26:49 +0530

    Add std::make_heap usage.

--- 001_make_heap/main.cpp
+++ 001_make_heap/main.cpp
@@ -1,7 +1,36 @@
+/*
+ * Author: Murtaza Sidhpurwala
+ * Date: 2024-08-06
+ * Description: This program demonstrates the use of std::make_heap to convert 
+ * a range of elements into a heap.
+ * 
+ * Important points : 
+ * 1.) std::make_heap is used to convert a range of elements into a heap. 
+ * 2.) A heap is a way to organize elements in a way that the largest element 
+ *     is always at the root.
+ * 3.) std::make_heap should be used along with std::push_heap and 
+ *     std::pop_heap to maintain the heap property.
+ * 4.) It's useful for scenarios where you need to access the highest element 
+ *     in constant time, eg:-Task Scheduling, Dijkstra's Algorithm.
+ * 5.) Insertion and removal operations are performed in logarithmic time.
+ * 
+ *                            9
+ *                           / \
+ * {3, 1, 4, 1, 5, 9}  ==>  5   4  ==>  {9, 5, 4, 1, 1, 3} 
+ *                         / \ / \
+ *                        1  1 3
+ */
+
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main() 
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> v = {3, 1, 4, 1, 5, 9};
+    std::make_heap(v.begin(), v.end());
+    for (int i : v) {
+        std::cout << i << " ";
+    }
     return 0;
 }
